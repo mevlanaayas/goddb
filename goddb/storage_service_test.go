c83644950215984dbad5867d80f40eacb43be03a
mevlanaayas <mevlanaayas@gmail.com>
2021-10-23 00:11:19 +0300

    added: unit test with mock

--- goddb/storage_service_test.go
+++ goddb/storage_service_test.go
@@ -5,38 +5,75 @@ import (
 	"testing"
 )
 
-func TestNewStorageService(t *testing.T) {
-	type args struct {
-		repository         GetPutFlusher
-		persistenceService ReadWriter
-	}
-	tests := []struct {
-		name string
-		args args
-		want StorageService
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewStorageService(tt.args.repository, tt.args.persistenceService); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewStorageService() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+var (
+	readMock   func() (error, []byte)
+	writeMock  func([]byte) error
+	getMock    func(key string) (error, string)
+	getAllMock func() (error, map[string]string)
+	putMock    func(key, value string) error
+	putAllMock func(values map[string]string) error
+	flushMock  func() error
+)
+
+type readWriterMock struct {
+}
+
+func (receiver readWriterMock) Read() (error, []byte) {
+	return readMock()
+}
+
+func (receiver readWriterMock) Write(value []byte) error {
+	return writeMock(value)
+}
+
+type getPutFlusherMock struct {
+}
+
+func (receiver getPutFlusherMock) Put(key, value string) error {
+	return putMock(key, value)
+}
+
+func (receiver getPutFlusherMock) Get(key string) (error, string) {
+	return getMock(key)
+}
+
+func (receiver *getPutFlusherMock) Flush() error {
+	return flushMock()
+}
+
+func (receiver getPutFlusherMock) GetAll() (error, map[string]string) {
+	return getAllMock()
+}
+
+func (receiver *getPutFlusherMock) PutAll(values map[string]string) error {
+	return putAllMock(values)
 }
 
 func TestStorageService_Flush(t *testing.T) {
+	verifyCalled := false
+	flushMock = func() error {
+		verifyCalled = true
+		return nil
+	}
 	type fields struct {
 		repository         GetPutFlusher
 		persistenceService ReadWriter
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
+		name       string
+		fields     fields
+		wantErr    bool
+		wantCalled bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "flush should not return error when flusher returns nil error",
+			fields: fields{
+				repository:         &getPutFlusherMock{},
+				persistenceService: &readWriterMock{},
+			},
+			wantErr:    false,
+			wantCalled: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -44,8 +81,12 @@ func TestStorageService_Flush(t *testing.T) {
 				repository:         tt.fields.repository,
 				persistenceService: tt.fields.persistenceService,
 			}
-			if err := receiver.Flush(); (err != nil) != tt.wantErr {
+			err2 := receiver.Flush()
+			if err := err2; (err != nil) != tt.wantErr {
 				t.Errorf("Flush() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantCalled != verifyCalled {
+				t.Errorf("no call for Flush() method on Flusher ")
 			}
 		})
 	}
